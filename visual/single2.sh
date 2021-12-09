b59231eb15e4a93fbf5b574fbaf0e4bf9498baed
Yuanli <leozhu@umich.edu>
2021-12-09 15:51:05 -0500

    add test script in GVNPRE

--- visual/single2.sh
+++ visual/single2.sh
@@ -5,9 +5,10 @@
 ### usage: ./run.sh ${benchmark_name} ${input} 
 ### e.g., ./run.sh compress compress.in or ./run.sh simple
 
-PATH_MYPASS=./build/src/LLVMHW2.so ### Action Required: Specify the path to your pass ###
-NAME_MYPASS=-gvnpre ### Action Required: Specify the name for your pass ###
+PATH_MYPASS=../build/SPGVNPRE/LLVMHW2.so ### Action Required: Specify the path to your pass ###
+NAME_MYPASS=-spgvnpre ### Action Required: Specify the name for your pass ###
 BENCH=../test/${1}.c
+TIME_MEASURE=../res/${1}_spgvn.txt
 
 # INPUT=${2}
 
@@ -29,9 +30,12 @@ opt -mem2reg ${1}/${1}.bc -o ${1}/${1}_reg.bc
 # Instrument profiler
 opt -pgo-instr-gen -instrprof ${1}/${1}_reg.bc -o ${1}/${1}.prof.bc 
 # Generate binary executable with profiler embedded
-clang -fprofile-instr-generate ${1}/${1}.prof.bc -o ${1}/${1}.prof
-# Collect profiling data
-./${1}.prof < ../test/${1}.in
+# echo -e "\n\n\n1. Result for reg" > ${TIME_MEASURE}
+# echo -e "\n\n   compile" >> ${TIME_MEASURE}
+clang -fprofile-instr-generate ${1}/${1}.prof.bc -o ${1}/${1}.prof;
+# # Collect profiling data
+# echo -e "\n\n   run" >> ${TIME_MEASURE}
+# { time ./${1}.prof < ../test/${1}.in; } 2>> ${TIME_MEASURE}
 # Translate raw profiling data into LLVM data format
 llvm-profdata merge -output=pgo.profdata default.profraw
 
@@ -48,6 +52,12 @@ opt -o ${1}/${1}.pre.bc -pgo-instr-use -pgo-test-profile-file=pgo.profdata -load
 opt -dot-cfg < ${1}/${1}.pre.bc > /dev/null
 
 dot -Tpng .${1}.dot -o ${1}/${1}_pre.png
+
+# echo -e "\n\n\n2. Result for pre" >> ${TIME_MEASURE}
+# echo -e "\n\n   compile" >> ${TIME_MEASURE}
+# { time clang ${1}.pre.bc -o ${1}_pre; } 2>> ${TIME_MEASURE}
+# echo -e "\n\n   run" >> ${TIME_MEASURE}
+# { time ./${1}_pre < ../test/${1}.in; } 2>> ${TIME_MEASURE}
 
 # perform dead code elimination
 
