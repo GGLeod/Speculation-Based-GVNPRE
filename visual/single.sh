dd4d3b1226d8cd429e9bebf88ef453c7822b7728
y1haohuang <yihaoh@umich.edu>
2021-12-09 14:59:19 -0500

    measure time for spgvnpre

--- visual/single.sh
+++ visual/single.sh
@@ -30,12 +30,12 @@ opt -mem2reg ${1}/${1}.bc -o ${1}/${1}_reg.bc
 # Instrument profiler
 opt -pgo-instr-gen -instrprof ${1}/${1}_reg.bc -o ${1}/${1}.prof.bc 
 # Generate binary executable with profiler embedded
-# echo -e "\n\n\n1. Result for reg" > ${TIME_MEASURE}
-# echo -e "\n\n   compile" >> ${TIME_MEASURE}
-# { time clang -fprofile-instr-generate ${1}.prof.bc -o ${1}.prof; } 2>> ${TIME_MEASURE}
-# # Collect profiling data
-# echo -e "\n\n   run" >> ${TIME_MEASURE}
-# { time ./${1}.prof < ../test/${1}.in; } 2>> ${TIME_MEASURE}
+echo -e "\n\n\n1. Result for reg" > ${TIME_MEASURE}
+echo -e "\n\n   compile" >> ${TIME_MEASURE}
+{ time clang -fprofile-instr-generate ${1}/${1}.prof.bc -o ${1}/${1}.prof; } 2>> ${TIME_MEASURE}
+# Collect profiling data
+echo -e "\n\n   run" >> ${TIME_MEASURE}
+{ time ${1}/${1}.prof < ../test/${1}.in; } 2>> ${TIME_MEASURE}
 # Translate raw profiling data into LLVM data format
 llvm-profdata merge -output=pgo.profdata default.profraw
 
@@ -53,11 +53,11 @@ opt -dot-cfg < ${1}/${1}.pre.bc > /dev/null
 
 dot -Tpng .${1}.dot -o ${1}/${1}_pre.png
 
-# echo -e "\n\n\n2. Result for pre" >> ${TIME_MEASURE}
-# echo -e "\n\n   compile" >> ${TIME_MEASURE}
-# { time clang ${1}.pre.bc -o ${1}_pre; } 2>> ${TIME_MEASURE}
-# echo -e "\n\n   run" >> ${TIME_MEASURE}
-# { time ./${1}_pre < ../test/${1}.in; } 2>> ${TIME_MEASURE}
+echo -e "\n\n\n2. Result for pre" >> ${TIME_MEASURE}
+echo -e "\n\n   compile" >> ${TIME_MEASURE}
+{ time clang ${1}/${1}.pre.bc -o ${1}/${1}_pre; } 2>> ${TIME_MEASURE}
+echo -e "\n\n   run" >> ${TIME_MEASURE}
+{ time ${1}/${1}_pre < ../test/${1}.in; } 2>> ${TIME_MEASURE}
 
 # perform dead code elimination
 
@@ -66,11 +66,11 @@ opt -dce ${1}/${1}.pre.bc -o ${1}/${1}_final.bc
 opt -dot-cfg < ${1}/${1}_final.bc > /dev/null
 
 dot -Tpng .${1}.dot -o ${1}/${1}_final.png
-# echo -e "\n\n\n3. Result for final" >> ${TIME_MEASURE}
-# echo -e "\n\n   compile" >> ${TIME_MEASURE}
-# { time clang ${1}_final.bc -o ${1}_final; } 2>> ${TIME_MEASURE}
-# echo -e "\n\n   run" >> ${TIME_MEASURE}
-# { time ./${1}_final < ../test/${1}.in; } 2>> ${TIME_MEASURE}
+echo -e "\n\n\n3. Result for final" >> ${TIME_MEASURE}
+echo -e "\n\n   compile" >> ${TIME_MEASURE}
+{ time clang ${1}/${1}_final.bc -o ${1}/${1}_final; } 2>> ${TIME_MEASURE}
+echo -e "\n\n   run" >> ${TIME_MEASURE}
+{ time ${1}/${1}_final < ../test/${1}.in; } 2>> ${TIME_MEASURE}
 
 opt -o ${1}/${1}.merge.bc -load ../MERGE/build/src/LLVMHW2.so -mergeblock < ${1}/${1}_final.bc > /dev/null
 
@@ -78,6 +78,10 @@ opt -dot-cfg < ${1}/${1}.merge.bc > /dev/null
 
 dot -Tpng .${1}.dot -o ${1}/${1}_merge.png
 
-
+echo -e "\n\n\n4. Result for merge" >> ${TIME_MEASURE}
+echo -e "\n\n   compile" >> ${TIME_MEASURE}
+{ time clang ${1}/${1}.merge.bc -o ${1}/${1}_merge; } 2>> ${TIME_MEASURE}
+echo -e "\n\n   run" >> ${TIME_MEASURE}
+{ time ${1}/${1}_merge < ../test/${1}.in; } 2>> ${TIME_MEASURE}
 
 rm .*
